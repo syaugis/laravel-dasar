06d036d0a0393a179dddcfb485dace1ff8e36fcf
syaugis <m.syaugishahab@gmail.com>
2024-12-07 13:00:33 +0700

    Api Resource

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/books', [BookController::class, 'index']);
-Route::post('/books', [BookController::class, 'store']);
-Route::get('/books/{book}', [BookController::class, 'show']);
-Route::put('/books/{book}', [BookController::class, 'update']);
-Route::delete('/books/{book}', [BookController::class, 'destroy']);
+Route::apiResource('books', BookController::class);
