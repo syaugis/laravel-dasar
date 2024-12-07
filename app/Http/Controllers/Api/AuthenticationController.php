8371d286553528902e7cc03f4c3b16faba70af21
syaugis <m.syaugishahab@gmail.com>
2024-12-07 13:07:54 +0700

    Authentication: Register

---
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:250'],
+            'email' => ['required', 'email', 'max:250', 'unique:users'],
+            'password' => ['required', 'min:6'],
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'You have successfully registered'
+        ]);
+    }
+}
